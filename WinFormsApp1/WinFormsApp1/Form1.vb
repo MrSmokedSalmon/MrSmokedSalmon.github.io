c3e3adbac89c6da881864c2ba65437c3d61a4532
MrSmokedSalmon <thederpman43@gmail.com>
2021-05-05 14:46:50 +1000

    Added Winstate check. Fixed Bug where you could choose a tile that had already been chosen

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -23,7 +23,6 @@ Public Class Form1
                 With k
                     .Size = New Size(112, 112)
                     .Location = New Point(107 + (118 * x), -80 + (118 * y))
-                    .Team = ""
                 End With
                 Controls.Add(k)
                 x += 1
@@ -32,6 +31,15 @@ Public Class Form1
         Next
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For Each i In Board
+            For Each k In i
+                k.Text = ""
+            Next
+        Next
+        Box.Tem = False
+    End Sub
 End Class
 
 Public Class Box
@@ -39,42 +47,67 @@ Public Class Box
 
     Public Shared Tem As Boolean
 
-    Private teem As String
-
     Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Me.Click
-        If Tem Then
-            Me.Text = "O"
-            Tem = False
-        Else Tem = True
-            Me.Text = "X"
+        If (Me.Text = "") Then
+            If Tem Then
+                Me.Text = "O"
+                Tem = False
+            Else Tem = True
+                Me.Text = "X"
+            End If
+            CheckWin()
         End If
-        Check()
     End Sub
 
-    Public Sub Check()
+    Public Shared Sub CheckWin()
+        'Checks for any winstate of the middle Tile
         Dim y = 0
         For Each i In Form1.Board
             Dim x = 0
             For Each k In i
-                If (k.Location.X = 343 And k.Location.Y = 156) Then
-                    If (Form1.Board(y - 1)(x - 1).Text = k.Text And Form1.Board(y + 1)(x + 1).Text = k.Text) Then
-                        MsgBox("Win")
+                If (x = 1 And y = 1) Then
+                    Dim l = -1
+                    While l <= 1
+                        If (k.Text = "X" Or k.Text = "O") Then
+                            'checks the winstates of the middle tile
+                            If (Form1.Board(0)(l + 1).Text = k.Text And Form1.Board(2)((l * -1) + 1).Text = k.Text) Then
+                                MsgBox("WIN")
+                                Exit Sub
+                            End If
+                        End If
+                        l += 1
+                    End While
+                    If (k.Text = "X" Or k.Text = "O") Then
+                        If (Form1.Board(1)(0).Text = k.Text And Form1.Board(1)(2).Text = k.Text) Then
+                            MsgBox("WIN")
+                            Exit Sub
+                        End If
                     End If
                 End If
                 x += 1
-                'MsgBox(x)
             Next
             y += 1
-            'MsgBox(y)
         Next
-    End Sub
 
-    Public Property Team As String
-        Get
-            Return teem
-        End Get
-        Set(ByVal value As String)
-            teem = value
-        End Set
-    End Property
+        If Form1.Board(0)(0).Text = "X" Or Form1.Board(0)(0).Text = "O" Then
+            If ((Form1.Board(0)(0).Text = Form1.Board(0)(1).Text) And Form1.Board(0)(2).Text = Form1.Board(0)(1).Text) Then
+                MsgBox("WIN")
+                Exit Sub
+            End If
+            If ((Form1.Board(0)(0).Text = Form1.Board(1)(0).Text) And Form1.Board(2)(0).Text = Form1.Board(1)(0).Text) Then
+                MsgBox("WIN")
+                Exit Sub
+            End If
+        End If
+        If (Form1.Board(2)(2).Text = "X" Or Form1.Board(2)(2).Text = "O") Then
+            If ((Form1.Board(0)(2).Text = Form1.Board(1)(2).Text) And Form1.Board(2)(2).Text = Form1.Board(1)(2).Text) Then
+                MsgBox("WIN")
+                Exit Sub
+            End If
+            If ((Form1.Board(2)(0).Text = Form1.Board(2)(1).Text) And Form1.Board(2)(2).Text = Form1.Board(2)(1).Text) Then
+                MsgBox("WIN")
+                Exit Sub
+            End If
+        End If
+    End Sub
 End Class
