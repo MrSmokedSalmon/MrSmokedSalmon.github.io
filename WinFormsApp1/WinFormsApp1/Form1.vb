9e4c285245a96c34d8e52b78935a17bd04ee8d25
MrSmokedSalmon <thederpman43@gmail.com>
2021-05-13 14:28:05 +1000

    Added Website with version history

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -9,12 +9,14 @@ Public Module MyExtensions
 End Module
 
 Public Class Form1
+    'Monday 3/5/21
     Public Board()() As Box = {
         New Box(2) {New Box, New Box, New Box},
         New Box(2) {New Box, New Box, New Box},
         New Box(2) {New Box, New Box, New Box}
     }
 
+    'Monday 3/5/21
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim y = 1
         For Each i In Board
@@ -32,6 +34,7 @@ Public Class Form1
 
     End Sub
 
+    'Wednesday 5/5/21
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         For Each i In Board
             For Each k In i
@@ -42,6 +45,7 @@ Public Class Form1
     End Sub
 End Class
 
+'Sunday 2/5/21
 Public Class Box
     Inherits Button
 
@@ -58,7 +62,7 @@ Public Class Box
             CheckWin()
         End If
     End Sub
-
+    'Wednesday 5/5/21
     Public Shared Sub CheckWin()
         'Checks for any winstate of the middle Tile
         Dim y = 0
@@ -109,5 +113,43 @@ Public Class Box
                 Exit Sub
             End If
         End If
+    End Sub
+
+    Public Sub Check()
+
+    End Sub
+
+
+End Class
+
+Public Class AI
+    Public Sub AnalyseBoard()
+        Dim y = 0
+        For Each i In Form1.Board
+            Dim x = 0
+            For Each k In i
+                If (x = 1 And y = 1) Then
+                    Dim l = -1
+                    While l <= 1
+                        If (k.Text = "X" Or k.Text = "O") Then
+                            'checks the winstates of the middle tile
+                            If (Form1.Board(0)(l + 1).Text = k.Text And Form1.Board(2)((l * -1) + 1).Text = k.Text) Then
+                                MsgBox("WIN")
+                                Exit Sub
+                            End If
+                        End If
+                        l += 1
+                    End While
+                    If (k.Text = "X" Or k.Text = "O") Then
+                        If (Form1.Board(1)(0).Text = k.Text And Form1.Board(1)(2).Text = k.Text) Then
+                            MsgBox("WIN")
+                            Exit Sub
+                        End If
+                    End If
+                End If
+                x += 1
+            Next
+            y += 1
+        Next
     End Sub
 End Class
